2ab433be35e4ad13ad2e2e480dd3dbf306eefb05
Ismail1993 <32904657+Ismail1993@users.noreply.github.com>
2018-03-01 14:00:03 +0000

    Update taxi.cpp

--- taxi.cpp
+++ taxi.cpp
@@ -1,6 +1,5 @@
 #import <iostream>
 #import <cmath>
-#include <string>
 #include <vector>
 
 //IN THIS FUN LITTLE PROJECT WE'RE TRYING TO PREDICT SHARP TURNS NOW WHAT WE means
@@ -15,7 +14,8 @@ using namespace std;
 void read_coords(vector<int> &x, vector<int> &y);
 void calculate(vector<int> &x, vector<int> &y, vector <double> &phi);
 void clean_arrays(vector<int> &x, vector<int> &y, vector<double> &phi);
-string inspect(int a, vector <double> &phi);
+char inspect(int a, vector <double> &phi);
+void change_coords(int n, vector<int> &x, vector<int> &y, vector <double> &phi);
 
 int main(){
 //defining key variables.
@@ -24,55 +24,72 @@ int main(){
   vector<int> x;
   vector<int> y;
   vector <double> phi;
-  //phi.at(0) = NAN;
 
 //enter the number of test cases T.
   do {
     cin >> T;
-}
-  while (T < 1 or T > 50);
+}while (T < 1 or T > 50);
 
-//enter the number of moments N.
-  do {
-    cin >> N;
-}
-  while (N < 3 or N > 50);
 
-for(int z = 0; z < T; z++){
-  printf("Test case number %d : \n ", (z+1));
-  for(int i = 1; i < N+1; i++){
+  for(int z = 0; z < T; z++){
+	  //enter the number of moments N.
+	  do {
+	    cin >> N;
+	  }while (N < 3 or N > 50);
+
+	  for(int i = 0; i < N; i++){
 //coordinates to be read in.
-    read_coords(x, y);
-  }
+		  read_coords(x, y);
+	  }
 //lets calculate the angle of turn now and fill phi vector array
-  calculate(x, y, phi);
-//lets print out the coordinates.
-  for(vector<int>::size_type i = 1; i < x.size(); i++) {
-	  cout << x.at(i) << " " << y.at(i) << endl;
-  }
-//see the results of our journey. LAST VALUE IS MEANINGLESS. out of bound problem.
-  for(vector<int>::size_type i = 0; i < phi.size() - 1; i++) {
-  	cout << inspect(i, phi) << " ";
-  }
+	  calculate(x, y, phi);
+//see the results of our journey.
+	  for(vector<int>::size_type i = 0; i < phi.size(); i++) {
+
+		  if (inspect(i, phi) == 'n'){
+//n stands for the Nth moment that we will change.
+			  int n;
+			  cin >> n;
+
+//change coordinates & clear phi for new entries.
+			  change_coords(n, x, y, phi);
+
+//calculate new phi.
+			  calculate(x, y, phi);
+
+//lets print out the coordinates & output the result.
+			  for(vector<int>::size_type i = 0; i < x.size(); i++) {
+				  cout << x.at(i) << " " << y.at(i) << endl;
+			  }
+			  cout << 'n' << ' ' << inspect(i, phi) << endl;
+
+		  }else{
+	//signify that there's no need to change coordinates.
+		    for(vector<int>::size_type i = 0; i < x.size(); i++) {
+		  	  cout << x.at(i) << " " << y.at(i) << endl;
+		    }
+		    	cout << inspect(i, phi) << ' ' << 'y' << endl;
+		  }
 //finally lets clean our array for the second test phase.
-  clean_arrays(x, y, phi);
-
+	  clean_arrays(x, y, phi);
+  }
 }
-
   return 0;
 
 }
 
 
-string inspect(int a, vector <double> &phi){
+char inspect(int a, vector <double> &phi){
 		//lets inspect each element in the vector array phi.
-	if(phi[a] > (M_PI / 4)){
-		return "no";
-		} else if(phi[a] < (M_PI / 4)){
-			return "yes";
-		}
-}
+	char result = '\0';
 
+	if(phi[a] > 0.79){
+		result = 'n';
+		}else if (phi[a] < 0.79){
+		result = 'y';
+		}
+	return result;
+}
 
 
 void calculate(vector<int> &x, vector<int> &y, vector <double> &phi){
@@ -80,7 +97,7 @@ void calculate(vector<int> &x, vector<int> &y, vector <double> &phi){
 
 	double phii;
 
-	for(vector<int>::size_type i = 1; i < x.size(); i++) {
+	for(vector<int>::size_type i = 1; i < x.size() - 1; i++) {
       phii = acos(((x[i] - x[i-1])*(x[i+1] - x[i])+(y[i] - y[i-1])*(y[i+1] - y[i]))/sqrt((pow((x[i] - x[i-1]), 2) + pow((y[i] - y[i-1]), 2))*(pow((x[i+1] - x[i]), 2) + pow((y[i+1] - y[i]), 2))));
       phi.push_back(phii);
   }
@@ -99,8 +116,21 @@ void read_coords(vector<int> &x, vector<int> &y){
 
 }
 
+void change_coords(int n, vector<int> &x, vector<int> &y, vector <double> &phi){
+	int ex;
+	int ey;
+	//type in new x,y coordinates.
+	cin >> ex;
+	cin >> ey;
+	//reflect that change in the x,y array of coordinates. 'n - 1' because indicies start from zero.
+	x.at(n - 1) = ex;
+	y.at(n - 1) = ey;
+	//call in calculate to recalculate our phi's.
+	phi.clear();
+}
+
 void clean_arrays(vector<int> &x, vector<int> &y, vector<double> &phi){
-  x.clear();
-  y.clear();
-  phi.clear();
+	x.clear();
+	y.clear();
+	phi.clear();
 }
